5d99a9f403c95c384aea49b0287da05a5b24901d
pkBilalAhmad <pk92love@gmail.com>
2016-05-08 23:21:20 +0500

    this is my website data

---
+++ public/js/myquery.js
@@ -0,0 +1,30 @@
+var btn=$("button");
+var p=$("p");
+var header=$("header");
+var section=$("section");
+var nav=$("nav");
+var article=$("article");
+var footer=$("footer");
+var ul=$("ul");
+var div=$("div");
+var menubtn=$("#menubtn");
+var fllft=$("#fllft");
+var showmenu=$("#showmenu");
+var hidemenu=$("#hidemenu");
+var dcmt=$("document");
+$(document).ready(function(){
+    $("#hidemenu").click(function(){
+        $("#showmenu").show();
+        $("#fllft").hide();
+        $("#hidemenu").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#showmenu").click(function(){
+        $("#hidemenu").show();
+        $("#fllft").show();
+        $("#showmenu").hide();
+    });
+});
+
